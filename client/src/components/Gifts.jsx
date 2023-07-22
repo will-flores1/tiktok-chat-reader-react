8755bd08664b05670c434a7aba1992b32ba4dc8b
wilfredo-flores <willflores1221@gmail.com>
2023-07-22 14:58:58 -0400

    Add: gift component & data filter

--- client/src/components/Gifts.jsx
+++ client/src/components/Gifts.jsx
@@ -2,34 +2,111 @@
 import { useEffect, useRef, useState } from "react";
 
 export function Gifts({ gifts }) {
+	const ulRef = useRef(null);
+
+	// Scroll to the bottom when new messages come in and the user is not scrolling up
+	useEffect(() => {
+		if (ulRef.current) {
+			ulRef.current.scrollTop = ulRef.current.scrollHeight;
+		}
+	}, [gifts]);
+
 	const styles = {
 		wrapper: {
 			display: "flex",
 			flexDirection: "column",
-			flexGrow: "1",
+			width: "50%",
 			justifyContent: "space-between",
 		},
 		listStyle: {
+			display: "flex",
+			flexDirection: "column",
+			gap: "14px",
 			listStyleType: "none",
 			overflowY: "scroll",
 			overflowX: "hidden",
 			height: "calc(100vh - 240px)",
 			padding: "0",
 			margin: "0",
-			lineHeight: "2",
 		},
 		listItem: {
 			display: "flex",
+			flexDirection: "column",
+		},
+		profilePicture: {
+			width: "24px",
+			height: "24px",
+			borderRadius: "50%",
+			marginRight: "5px",
+		},
+		text: {
+			display: "flex",
+			alignItems: "center",
+			lineHeight: "1.3",
+		},
+		gift: {
+			display: "flex",
 			alignItems: "center",
 		},
+		giftPicture: {
+			width: "48px",
+			height: "48px",
+		},
+		textWrapper: {
+			display: "flex",
+			flexDirection: "column",
+			justifyContent: "space-between",
+			marginLeft: "10px",
+			lineHeight: "1.3",
+		},
 	};
+
+	// filter repeat gifts objects with same giftId
+	const filteredGifts = gifts.filter(
+		(gift, index, self) =>
+			index === self.findIndex((t) => t.groupId === gift.groupId)
+	);
 
 	return (
 		<div style={styles.wrapper}>
 			<h2>Gifts</h2>
-			<ul style={styles.listStyle}>
-				{gifts.map((event, index) => (
-					<li key={index}>{event.giftName}</li>
+			<ul ref={ulRef} style={styles.listStyle}>
+				{filteredGifts.map((event, index) => (
+					<li key={index} style={styles.listItem}>
+						<div style={styles.text}>
+							<img
+								style={styles.profilePicture}
+								src={event.profilePictureUrl}
+								alt="gifter profile picture"
+							/>
+							<span style={{ color: "#36a2eb" }}>{event.uniqueId}</span>
+							<span>: sent {event.giftName}</span>
+						</div>
+						<div style={styles.gift}>
+							<img
+								style={styles.giftPicture}
+								src={event.giftPictureUrl}
+								alt="gift picture"
+							/>
+							<div style={styles.textWrapper}>
+								<p>
+									name:{" "}
+									<span style={{ fontWeight: 800 }}>{event.giftName}</span>
+									<span> (ID:{event.giftId})</span>
+								</p>
+								<p>
+									repeat:{" "}
+									<span style={{ fontWeight: 800 }}>{event.repeatCount}x</span>
+								</p>
+								<p>
+									cost:{" "}
+									<span style={{ fontWeight: 800 }}>
+										{event.diamondCount * event.repeatCount} coins
+									</span>
+								</p>
+							</div>
+						</div>
+					</li>
 				))}
 			</ul>
 		</div>
